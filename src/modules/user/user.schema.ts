a79fd82d45e9e2642fa7d4d59aae3bd90e345d96
officefish <indicogames@yandex.ru>
2023-03-14 14:57:17 -0700

    tutorial done

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,31 +1,49 @@
 import { buildJsonSchemas } from 'fastify-zod'
 import {z} from 'zod'
 
-const useCore = {
+const email = {
     email: z.string({
         required_error: 'Email is required',
         invalid_type_error: 'Email must be a string',
-    }).email(),
-    name: z.string(),
+    }).email()
 }
-
-const createUserSchema = z.object({
-    ...useCore,
+const password = {
     password: z.string({
         required_error: 'Password is required',
         invalid_type_error: 'Password must be a string',
     })
+}
+const name = { name: z.string() }
+const id = { id: z.string() }
+
+const createUserSchema = z.object({
+    ...email,
+    ...password,
+    ...name,
 })
 
 const createUserResponseSchema = z.object({
-    id: z.string(),
-    ...useCore,
+    ...id,
+    ...name,
+    ...email,
+})
+
+const loginUserSchema = z.object({
+    ...email,
+    ...password,
+})
+
+const loginUserResponseSchema = z.object({
+    accessToken: z.string(),
 })
 
 type CreateUserInput = z.infer<typeof createUserSchema>
-export { CreateUserInput }
+type LoginInput = z.infer<typeof loginUserSchema>
+export { CreateUserInput, LoginInput }
 
 export const {schemas:UserSchemas, $ref} = buildJsonSchemas({
     createUserSchema,
-    createUserResponseSchema
-})
+    createUserResponseSchema,
+    loginUserSchema,
+    loginUserResponseSchema
+}, {$id: 'UserSchema'})
