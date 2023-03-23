e00e7d12d47d7d05315d7f18b31e34e70fd799c4
officefish <indicogames@yandex.ru>
2023-03-23 09:15:06 -0700

    some testing progress

--- test/user/api.test.ts
+++ test/user/api.test.ts
@@ -1,23 +1,26 @@
 "use strict"
 import { describe, it, test, beforeAll, afterAll, expect, } from 'vitest'
+import { faker } from '@faker-js/faker'
 import supertest from 'supertest'
 import {buildApp, AppOptions} from '../../src/app'
 
 const options: AppOptions = { logger: false, }
 const contentType = 'application/json; charset=utf-8'
 
-describe('Create User API', () => {
+const users = '/api/users' 
 
-    let app
-    beforeAll(async () => {
-        app = await buildApp(options)
-    })
+let app: Awaited<ReturnType<typeof buildApp>>
+beforeAll(async () => { 
+    app = await buildApp(options) }
+)
+afterAll(() => { app.close() })
 
-    it('should response error for empty body request', async () => {
-        const response = await app.inject({
-            method: 'POST',
-            url: '/api/users',
-        })
+describe('User API', () => {
+
+    it('POST /api/users with empty body', async () => {
+        const response = await app.inject()
+            .post(users)
+        
 
         expect(response.statusCode).toBe(400)
         expect(response.headers['content-type']).toBe(contentType)
@@ -26,8 +29,74 @@ describe('Create User API', () => {
         expect(response.json()).haveOwnProperty('message')
     })
 
-    afterAll(async () => {
-        await app.close()
+
+    it('POST /api/users with minimum required', async () => {
+        const payload = {
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+        const response = await app.inject()
+            .post(users)
+            .payload(payload)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.headers['content-type']).toBe(contentType)
+        expect(response.json()).instanceOf(Object)
+        expect(response.json()).haveOwnProperty('access-token')
+        expect(response.json()).haveOwnProperty('verified')
+        expect(response.json().verified).is.false
+        /* Verify jwt token test */
+        // const verify = jwt.verify as jest.MockedFunction<(
+        // token: string,
+        // secretOrPublicKey: jwt.Secret,
+        // options?: jwt.VerifyOptions,
+        // ) => Record<string, unknown> | string
+        // verify.mockReturnValue({ verified: 'true' })
     })
 
+    it('GET /api/users/ verify/:email/:expires/:token', async () => {
+
+
+        expect(true).toBeTruthy()
+
+        
+        // const payload = {
+        //     email: faker.internet.email(),
+        //     password: faker.internet.password(),
+        // }
+
+        // const domain = 'localhost:8001/api/auth'
+        // const link_expires = request.server.env.LINK_EXPIRE_MINUTES
+        // const expires = service.NowPlusMinutes(link_expires).getTime().toString()
+   
+
+        // const crypto = request.server.minCrypto
+        // const signature = request.server.env.JWT_SIGNATURE
+
+        // const emailToken = await service.CreateJwt(crypto, {signature, email, expires}, ':')
+
+        // const encodedEmail = encodeURIComponent(email)
+        // const link =
+        //     `http://${domain}/verify/` + `${encodedEmail}/${expires}/${emailToken}`
+
+        // //const unencodedEmail = 
+
+        // const response = await app.inject()
+        //     .get(users)
+        //     //.pa(payload)
+
+        // expect(response.statusCode).toBe(200)
+        // expect(response.headers['content-type']).toBe(contentType)
+        // expect(response.json()).instanceOf(Object)
+        // expect(response.json()).haveOwnProperty('access-token')
+        // expect(response.json()).haveOwnProperty('verified')
+        // expect(response.json().verified).is.false
+        /* Verify jwt token test */
+        // const verify = jwt.verify as jest.MockedFunction<(
+        // token: string,
+        // secretOrPublicKey: jwt.Secret,
+        // options?: jwt.VerifyOptions,
+        // ) => Record<string, unknown> | string
+        // verify.mockReturnValue({ verified: 'true' })
+    })
 })
